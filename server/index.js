7d6531c956bf94d83926e7defc85c0be0f8fbc7c
Bhavya <bhavya7664@gmail.com>
2024-02-28 00:21:39 +0530

    new

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://sellox-frontend-pi.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
