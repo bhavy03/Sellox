0928aab3b3f006f0fba4fa54e558b2254c65c677
Bhavya <bhavya7664@gmail.com>
2024-03-02 13:50:03 +0530

    new

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,7 +14,11 @@ const Home = () => {
     useEffect(() => {
         async function fetchData() {
             try {
-                const response = await fetch(`${import.meta.env.VITE_SERVER}card/all`);
+                const response = await fetch(`${import.meta.env.VITE_SERVER}card/all`, {
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                    }
+                });
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
